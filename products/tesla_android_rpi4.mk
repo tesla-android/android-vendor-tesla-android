554a72fca8a72ef286cdff550e2d1d51c1db6023
Michał Gapiński <mike@gapinski.eu>
2023-05-01 19:05:44 +0200

    Switch ota flavor url

--- products/tesla_android_rpi4.mk
+++ products/tesla_android_rpi4.mk
@@ -15,6 +15,6 @@ PRODUCT_MANUFACTURER := RaspberryPiFoundation
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.tesla-android.device=rpi4 \
-    tesla-android.updater.uri=https://ota.teslaandroid.com/api/v1/rpi4/official \
+    tesla-android.updater.uri=https://ota.teslaandroid.com/api/v1/rpi4/release \
 
 GD_LCD_DENSITY = 200
