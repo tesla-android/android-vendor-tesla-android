37e0e0cf11ac767d7cb4b63e552fe55eac374794
Michał Gapiński <mikegapinski@users.noreply.github.com>
2023-05-22 19:52:36 +0200

    2023.20.1.1

--- vendor.mk
+++ vendor.mk
@@ -6,7 +6,7 @@ PRODUCT_PACKAGES += \
     Updater \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.tesla-android.build.version=2023.20.1 \
+    ro.tesla-android.build.version=2023.20.1.1 \
     ro.tesla-android.releasetype=RELEASE \
     tesla-android.updater.allow_downgrading=true \
 
