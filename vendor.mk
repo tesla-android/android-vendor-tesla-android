f13ce5431befabebf606e0f6178c757b41f69373
Michał Gapiński <mike@gapinski.eu>
2023-08-15 16:20:07 +0200

    Frontend update, 2023.32.1

--- vendor.mk
+++ vendor.mk
@@ -3,7 +3,7 @@
 # Version
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.tesla-android.build.version=2023.32.1.0 \
+    ro.tesla-android.build.version=2023.32.1 \
 
 ################################################################################
 
