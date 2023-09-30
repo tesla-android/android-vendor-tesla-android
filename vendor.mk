6f7cd5d0fc1dbf96b813885ebe7df9f3e1bff419
Michał Gapiński <mike@gapinski.eu>
2023-09-30 12:04:28 +0200

    Bump version

--- vendor.mk
+++ vendor.mk
@@ -3,7 +3,7 @@
 # Version
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.tesla-android.build.version=2023.38.1 \
+    ro.tesla-android.build.version=2023.39.0.1 \
 
 ################################################################################
 
