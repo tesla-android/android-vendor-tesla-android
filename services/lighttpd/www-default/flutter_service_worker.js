2f6f0bf75d8fc28d054ad4c8554ac4719f198cb2
Michał Gapiński <mike@gapinski.eu>
2023-10-21 13:47:35 +0200

    frontend update

--- services/lighttpd/www-default/flutter_service_worker.js
+++ services/lighttpd/www-default/flutter_service_worker.js
@@ -4,13 +4,13 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"h264.js": "36ea412ec86751be2a5d7a14aafc5e07",
-"version.json": "8989b8671a361555a999504c1dceadf9",
+"version.json": "8809723bf106130a83c66af63de838c6",
 "favicon.ico": "1a96a32111338f8aa97efc700d8a7612",
-"index.html": "fbd6ddcfbe5e9023ebf5d673e2c3b42c",
-"/": "fbd6ddcfbe5e9023ebf5d673e2c3b42c",
+"index.html": "bd1a6cfa1f19aadee90fa5d56ed9df9c",
+"/": "bd1a6cfa1f19aadee90fa5d56ed9df9c",
 "imgTag.js": "a5870d73cba2f73c28b9b5f0820d93b9",
 "workerWebGLWebCodecs.js": "7f3bff3e2bb1cd5190fe257a6ab19def",
-"main.dart.js": "8de96a2cc4eb53fd7bc5a52a4cf2d876",
+"main.dart.js": "9023164fabf289ea89b82e7d6b69fdc4",
 "reconnecting-websocket.js": "4ef9dd81c9e8157a1702623078c7ece1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "estimator.js": "2e8d77774febade25070f24689662912",
