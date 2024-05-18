2d340b35683eafc28da32c8d271ac7a3c98b0eaf
Michał Gapiński <mike@gapinski.eu>
2024-05-18 11:26:11 +0200

    Frontend update

--- vendor.mk
+++ vendor.mk
@@ -255,8 +255,8 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/services/lighttpd/www-default/canvaskit/chromium/canvaskit.js.symbols:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/canvaskit/chromium/canvaskit.js.symbols \
      $(LOCAL_PATH)/services/lighttpd/www-default/canvaskit/skwasm.js.symbols:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/canvaskit/skwasm.js.symbols \
      $(LOCAL_PATH)/services/lighttpd/www-default/flutter_bootstrap.js:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/flutter_bootstrap.js \
-     $(LOCAL_PATH)/services/lighttpd/www-default/main.dart.mjs:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/main.dart.mjs \
-     $(LOCAL_PATH)/services/lighttpd/www-default/main.dart.wasm:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/main.dart.wasm \
+#     $(LOCAL_PATH)/services/lighttpd/www-default/main.dart.mjs:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/main.dart.mjs \
+#     $(LOCAL_PATH)/services/lighttpd/www-default/main.dart.wasm:$(TARGET_COPY_OUT_VENDOR)/tesla-android/lighttpd/www-default/main.dart.wasm \
 
 ################################################################################
 
